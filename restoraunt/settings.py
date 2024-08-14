bd913ca9e15686a71fd547c1a49378712178fb56
taras8077 <taras8077@gmail.com>
2024-08-14 19:52:56 +0300

    +order

--- restoraunt/settings.py
+++ restoraunt/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hv)ic!uu447txm9q(rx_dvzj364m$^ohn283cj$$)3a^*sy8(='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hv)ic!uu447txm9q(rx_dvzj364m$^ohn283cj$$)3a^*sy8(=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
